15c6a74ae0ad9efc55de97f1a0cdbb8846770612
xqgdtx1@sina.com <xqgdtx1@sina.com>
2024-03-10 11:27:24 +0800

    加入AWS学习

---
+++ saa-learn/src/test/java/learn/saalearn/SaaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.saalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
